441eeab994cc5757990a9ac6265c8060cd0a1008
Fedddix <danilafedoseev99@gmail.com>
2023-04-18 00:25:08 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
-const body = document.querySelector("body"),
-switchMode = document.querySelector('#switchMode');
-
-// let getMode = localStorage.getItem("mode");
-// if (getMode && getMode === "dark") {
-//   body.classList.add("dark");
-//   switchMode.classList.add("active");
-// }
-
+const body = document.querySelector("body");
+const switchMode = document.querySelector('.switchMode');
 switchMode.addEventListener("click", () => {
-  body.classList.switchMode("dark");
-
-  if (!body.classList.contains("dark")) {
-    return localStorage.setItem("mode", "light");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
   }
-  localStorage.setItem("mode", "dark");
 });
 
-switchMode.addEventListener("click", () => switchMode.classList.switchMode("active"));
+// ВОЗМОЖНО НЕ НУЖНО
+// const darkmode_dark = document.getElementById("darkmode_dark");
+// const like_dark = document.getElementById("like_dark");
+// const insta_dark = document.getElementById("insta_dark");
+// const faceb_dark = document.getElementById("faceb_dark");
+
+darkmode_dark.addEventListener('click', function () {
+  if (darkmode_dark.classList.contains("darkmode_dark")) {
+    darkmode_dark.classList.remove("darkmode_dark");
+    like_dark.classList.remove("like_dark");
+    insta_dark.classList.remove("insta_dark");
+    faceb_dark.classList.remove("faceb_dark");
+  } else {
+    darkmode_dark.classList.add("darkmode_dark");
+    like_dark.classList.add("like_dark");
+    insta_dark.classList.add("insta_dark");
+    faceb_dark.classList.add("faceb_dark");
+  }
+})
+
