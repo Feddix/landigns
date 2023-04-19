cabea3da52f57010bc5deea46287a90a41eab575
Feddix <danilafedoseev99@gmail.com>
2023-04-19 13:29:12 +0300

    dark_mode2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 const body = document.querySelector("body");
+
 const switchMode = document.querySelector('.switchMode');
 switchMode.addEventListener("click", () => {
   if (body.classList.contains("dark")) {
     body.classList.remove("dark");
+    
   } else {
     body.classList.add("dark");
+    ;
+
   }
 });
+const text = document.querySelectorAll("p");
 
-// ВОЗМОЖНО НЕ НУЖНО
-// const darkmode_dark = document.getElementById("darkmode_dark");
-// const like_dark = document.getElementById("like_dark");
-// const insta_dark = document.getElementById("insta_dark");
-// const faceb_dark = document.getElementById("faceb_dark");
+switchMode.addEventListener("click", function () {
+  if(body.classList.contains("dark")){
+    text.style.color = 'black';
+  } else {
+    text.style.color = 'white';
+
+  }
+}
+)
+
 
 darkmode_dark.addEventListener('click', function () {
   if (darkmode_dark.classList.contains("darkmode_dark")) {
@@ -25,6 +35,8 @@ darkmode_dark.addEventListener('click', function () {
     like_dark.classList.add("like_dark");
     insta_dark.classList.add("insta_dark");
     faceb_dark.classList.add("faceb_dark");
+
   }
 })
+
 
